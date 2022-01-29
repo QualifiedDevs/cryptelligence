4ebdb5b5475a3d526d6147f276626079073a950e
cryptominnow <minnow@spookysouls.com>
2022-01-29 05:29:19 -0600

    another ignore

---
+++ src/FAQ.tsx
@@ -0,0 +1,12 @@
+import {styled} from '@mui/material/styles';
+import {Box} from '@mui/material';
+
+const FAQ = styled((props) => {
+    return (
+        <Box {...props}>
+            
+        </Box>
+    );
+})``;
+
+export default FAQ;
